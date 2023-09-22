86face1b01d82c94666e82ca1ef20fec73909850
carolina arruda <carolinacalouroarruda@gmail.com>
2023-09-22 15:11:34 +0200

    working design for top nav bar

--- src/components/navbar/NavBarTop.js
+++ src/components/navbar/NavBarTop.js
@@ -1,27 +1,50 @@
-import classes from './NavBarTop.module.css'
+import classes from "./NavBarTop.module.css";
+
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+import ArrowDown from "../svgs/ArrowDown";
 
 const NavBarTop = () => {
-    return (
-        <>
-            <div className={classes.NavBarTopContainer}>
-                <ul>
-                    <li></li>
-                    <li></li>
-                </ul>
-                <p>Language <span>^</span> </p>
-                <div className={classes.languages}>
-                    <p>English</p>
-                    <p>French</p>
-                </div>
-                <p>Currency</p>
-                <div className={classes.currencies}>
-                    <p id='pound'>£</p>
-                    <p id='dollar'>$</p>
-                    <p id='euro'>€</p>
-                </div>
+  return (
+    <>
+      <div className={classes.topNav}>
+        <div className={classes.lefty}>
+          <p>SHIPPING</p>
+          <p>CONTACT</p>
+        </div>
+        <div className={classes.righty}>
+          <div>
+            <p className={classes.dropdown}>
+              LANGUAGE
+              <span className={classes.arrow}>
+                <ArrowDown />
+              </span>
+            </p>
+          </div>
+          <div>
+            <div className={classes.languages}>
+              {/* <p>English</p>
+              <p>French</p> */}
             </div>
-        </>
-    )
-}
 
-export default NavBarTop
+            <div>
+              <p>
+                CURRENCY
+                <span className={classes.arrow}>
+                  <ArrowDown />
+                </span>
+              </p>
+              {/* <div className={classes.currencies}>
+                <p id="pound">£</p>
+                <p id="dollar">$</p>
+                <p id="euro">€</p>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBarTop;
