4b1a35181a42f8d3e3ba978b69b1a8a54bc02dc8
carolina arruda <carolinacalouroarruda@gmail.com>
2023-09-27 12:41:16 +0200

    working on badges

--- src/components/footer/FooterTop.js
+++ src/components/footer/FooterTop.js
@@ -1,15 +1,29 @@
-import classes from "./FooterTop.module.css"
+import classes from "./FooterTop.module.css";
+import Delivery from "../svgs/Delivery";
 
 const FooterTop = () => {
-    return (
-        <>
-            <section className={classes.words}>
-                <div><p>hello</p></div>
-                <div><p>hello</p></div>
-                <div><p>hello</p></div>
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <section className={classes.words}>
+        <div className={classes.badges}>
+          <div className={classes.delivery}>
+            <Delivery />
+          </div>
+
+          <div className={classes.badgeTitle}>
+            <h5>FAST AND FREE DELIVERY</h5>
+            <p>Free delivery for all orders over $140</p>
+          </div>
+        </div>
+        <div>
+          <p>hello</p>
+        </div>
+        <div>
+          <p>hello</p>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default FooterTop;
