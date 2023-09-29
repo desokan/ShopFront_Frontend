67d43be1a1aa05453cbf041f02ca8b26a8d31a3a
carolina arruda <carolinacalouroarruda@gmail.com>
2023-09-29 12:31:54 +0200

    finsihed bottom

--- src/components/footer/FooterBot.js
+++ src/components/footer/FooterBot.js
@@ -1,9 +1,30 @@
+import classes from "./FooterBot.module.css";
+import ArrowUp from "../svgs/ArrowUp";
+
 const FooterBot = () => {
-    return (
-        <>
-        
-        </>
-    )
-}
+  return (
+    <section className={classes.footerBotContainer}>
+      <div>
+        <p>©2023 Uomo</p>
+      </div>
+      <div className={classes.optionsContainer}>
+        <p>Language</p>
+        <p>
+          United Kingdom | English
+          <span className={classes.arrowUp}>
+            <ArrowUp />
+          </span>
+        </p>
+        <p>Currency</p>
+        <p>
+          $ USD
+          <span className={classes.arrowUp}>
+            <ArrowUp />
+          </span>
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default FooterBot;
