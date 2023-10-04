87fe71c962177a69af9bdc0104eaa07011180f69
Isabelle <isabel.tartarelli@live.it>
2023-10-04 12:53:48 +0100

    Created files for Recently Viewed. Displayed on screen. Trying to fix @media

--- src/components/mostPreferred/mostPreferred.js
+++ src/components/mostPreferred/mostPreferred.js
@@ -1,19 +1,27 @@
 import classes from './mostPreferred.module.css'
 import SingleCardMostPreferres from './singleCardMostPreferred'
-const MostPreferred = () =>{
-    return (
-        <div className={classes.MostPreferredContainer}>
-            <div className={classes.MostPreferredTitleContainer}>
-                <h2 className={classes.MostPreferredTitleH2}> MOST PREFERRED CATEGORIES</h2>
-                <p className={classes.MostPreferredTitleP}>The World's Premium Brands In One Destination.</p>
-            </div>
-            <div className={classes.MostPreferredCards}>
-                <SingleCardMostPreferres/>
-                <SingleCardMostPreferres/>
-                <SingleCardMostPreferres/>
-                <SingleCardMostPreferres/>
-            </div>
-        </div>
-    )
+
+const MostPreferred = () => {
+	return (
+		<div className={classes.MostPreferredContainer}>
+			<div className={classes.MostPreferredTitleContainer}>
+				<div className={classes.MostPreferredTitle}>
+					<h2 className={classes.MostPreferredTitleH2}>
+						{' '}
+						MOST PREFERRED CATEGORIES
+					</h2>
+					<p className={classes.MostPreferredTitleP}>
+						The World's Premium Brands In One Destination.
+					</p>
+				</div>
+			</div>
+			<div className={classes.MostPreferredCards}>
+				<SingleCardMostPreferres />
+				<SingleCardMostPreferres />
+				<SingleCardMostPreferres />
+				<SingleCardMostPreferres />
+			</div>
+		</div>
+	)
 }
 export default MostPreferred
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,21 @@
-import NavBar from "../components/navbar/NavBarContainer";
-import MainSlider from "../components/heroBanner/heroBanner";
-import WeeklyFeatured from "../components/weeklyFeatured/weeklyFeatured";
-import Footer from "../components/footer/FooterContainer";
+import NavBar from '../components/navbar/NavBarContainer'
+import MainSlider from '../components/heroBanner/heroBanner'
+import WeeklyFeatured from '../components/weeklyFeatured/weeklyFeatured'
+import Footer from '../components/footer/FooterContainer'
+import MostPreferred from '../components/mostPreferred/mostPreferred'
+import RecentlyViewed from '../components/recentlyViewed/RecentlyViewed'
 
 const HomePage = () => {
-    return (
-        <>
-            <NavBar/>
-            <MainSlider/>
-            <WeeklyFeatured/>
-            <Footer/>
-        </>
-    )
+	return (
+		<>
+			<NavBar />
+			<MainSlider />
+			<WeeklyFeatured />
+			<MostPreferred />
+            <RecentlyViewed />
+			<Footer />
+		</>
+	)
 }
 
-export default HomePage;
+export default HomePage
