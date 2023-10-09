017d3e6acde212bc836704dd552db7fe870ab081
Mantas Navickis <navickis@gmail.com>
2023-10-09 13:12:25 +0200

    Changed file names and paths

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,9 @@
 import NavBar from '../components/navbar/NavBarContainer'
 import MainSlider from '../components/heroBanner/HeroBanner'
-import WeeklyFeatured from '../components/weeklyFeatured/weeklyFeatured'
+import WeeklyFeatured from '../components/weeklyFeatured/WeeklyFeatured'
 import Footer from '../components/footer/FooterContainer'
-import MostPreferred from '../components/mostPreferred/mostPreferred'
 import RecentlyViewed from '../components/recentlyViewed/RecentlyViewed'
+import MostPreferred from '../components/mostPreferred/MostPreferred'
 
 const HomePage = () => {
 	return (
@@ -11,7 +11,7 @@ const HomePage = () => {
 			<NavBar />
 			<MainSlider />
 			<WeeklyFeatured />
-			<MostPreferred />
+			<MostPreferred/>
       <RecentlyViewed />
 			<Footer />
 		</>
