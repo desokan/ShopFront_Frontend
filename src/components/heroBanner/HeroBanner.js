d0f75f33809c342f1d2abdfec919818bf78481f0
Isabelle <isabel.tartarelli@live.it>
2023-10-11 13:38:20 +0100

    Fixed positioning

--- src/components/heroBanner/HeroBanner.js
+++ src/components/heroBanner/HeroBanner.js
@@ -1,20 +1,30 @@
 import React from 'react'
-import classes from "./HeroBanner.module.css";
-import ArrowLeft from './svgs/ArrowLeft';
-import ArrowRight from './svgs/ArrowRight';
-
+import classes from './HeroBanner.module.css'
+import ArrowLeft from './svgs/ArrowLeft'
+import ArrowRight from './svgs/ArrowRight'
 
 const MainSlider = () => {
 	return (
 		<div className={classes.heroBannerContainer}>
-      <div className={classes.heroBannerTextContainer}>
-       <h2 className={classes.heroBannerTextContainerH1}>NATURAL GLOW</h2>
-       <p className={classes.heroBannerTextContainerP}>Beaux products protect, moisturize, and lubricate your skin. It smartly nourish your skin with lotions, day creams, night creams, tinted moisturizers, and more.</p>
-      <button className={classes.heroBannerTextContainerButton}>VIEW MORE</button>
-      </div>
-      <div className={classes.heroBannerArrowLeft}><ArrowLeft/></div>
-      <div className={classes.heroBannerArrowRight}><ArrowRight/></div>
-    </div>
+			<div className={classes.heroBannerArrowLeft}>
+				<ArrowLeft />
+			</div>
+			<div className={classes.heroBannerTextContainer}>
+				<h2 className={classes.heroBannerTextContainerH1}>NATURAL GLOW</h2>
+				<p className={classes.heroBannerTextContainerP}>
+					Beaux products protect, moisturize, and lubricate your skin. It
+					smartly nourish your skin with lotions, day creams, night creams,
+					tinted moisturizers, and more.
+				</p>
+				<button className={classes.heroBannerTextContainerButton}>
+					VIEW MORE
+				</button>
+			</div>
+
+			<div className={classes.heroBannerArrowRight}>
+				<ArrowRight />
+			</div>
+		</div>
 	)
 }
 
--- src/components/weeklyFeatured/WeeklyFeatured.js
+++ src/components/weeklyFeatured/WeeklyFeatured.js
@@ -4,31 +4,30 @@ import SingleCard from './SingleCard'
 import ArrowRight from '../heroBanner/svgs/ArrowRight'
 
 const WeeklyFeaturedd = () => {
-  return (
-    <div className={classes.WeeklyFeaturedContainer}>
-      <div className={classes.WeeklyFeaturedArrowLeft}>
-        <ArrowLeft width='80px' height='80px' fill='none' />
-      </div>
-      <div className={classes.WeeklyFeaturedTitle}>
-        <h3 className={classes.WeeklyFeaturedTitleH3}>
-          WEEKLY FEATURED PRODUCTS
-        </h3>
-        <p className={classes.WeeklyFeaturedTitleP}>
-          The World's Premium Brands In One Destination.
-        </p>
-      </div>
-      <div className={classes.WeeklyFeaturedCards}>
-        <SingleCard/>
-        <SingleCard/>
-        <SingleCard/>
-        <SingleCard/>
-      </div>
-      <div className={classes.WeeklyFeaturedArrowRight}>
-        <ArrowRight width='80px' height='80px' fill='none' />
-      </div>
-    </div>
-  )
+	return (
+		<div className={classes.WeeklyFeaturedContainer}>
+			<div className={classes.WeeklyFeaturedArrowLeft}>
+				<ArrowLeft width="80px" height="80px" fill="none" />
+			</div>
+			<div className={classes.WeeklyFeaturedTitle}>
+				<h3 className={classes.WeeklyFeaturedTitleH3}>
+					WEEKLY FEATURED PRODUCTS
+				</h3>
+				<p className={classes.WeeklyFeaturedTitleP}>
+					The World's Premium Brands In One Destination.
+				</p>
+			</div>
+			<div className={classes.WeeklyFeaturedCards}>
+				<SingleCard />
+				<SingleCard />
+				<SingleCard />
+				<SingleCard />
+			</div>
+			<div className={classes.WeeklyFeaturedArrowRight}>
+				<ArrowRight width="80px" height="80px" fill="none" />
+			</div>
+		</div>
+	)
 }
-
 
 export default WeeklyFeaturedd
