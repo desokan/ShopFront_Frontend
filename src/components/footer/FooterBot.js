9d17657c5bcc5276e1ed89f9b9ade36dc73969a8
carolina arruda <carolinacalouroarruda@gmail.com>
2023-10-12 13:07:01 +0200

    finished translations footer

--- src/components/footer/FooterBot.js
+++ src/components/footer/FooterBot.js
@@ -2,16 +2,16 @@ import classes from "./FooterBot.module.css";
 import DropDownCurrency from "../dropDowns/DropDownCurrency";
 import DropDownLanguage from "../dropDowns/DropDownLanguage";
 
-const FooterBot = () => {
+const FooterBot = ({t}) => {
   return (
     <section className={classes.footerBotContainer}>
       <div>
         <p>©{new Date().getFullYear()} Uomo</p>
       </div>
       <div className={classes.optionsContainer}>
-        <p>LANGUAGE</p>
+        <p>{t("footer.Bottom.Language")}</p>
         <DropDownLanguage className="footer" />
-        <p>CURRENCY</p>
+        <p>{t("footer.Bottom.Currency")}</p>
         <DropDownCurrency className="footer" />
       </div>
     </section>
