53ff24e957814ef234aba670f8681181bc18b793
NoroAxper <norikvardanyan.w@gmail.com>
2023-10-12 11:50:05 +0100

    added translation func to navbar

--- src/components/dropDowns/DropDownLanguage.js
+++ src/components/dropDowns/DropDownLanguage.js
@@ -1,9 +1,11 @@
 import classes from "./DropDownLanguage.module.css"
 import { useState } from "react";
 import i18n from "../../i18n";
+import { useTranslation } from 'react-i18next'
 
 const DropDownLanguage = ({className}) => {
 
+  const { t } = useTranslation();
   const [selectedLanguage, setSelectedLanguage] = useState(i18n.language); // i18n.language contains the language assigned to lng in i18n.js file.
 
   const chooseLanguage = (e) => {
@@ -14,9 +16,9 @@ const DropDownLanguage = ({className}) => {
 
   return (
     <select defaultValue={selectedLanguage} onChange={chooseLanguage} className={(className === 'footer' ? classes.footerDropDown : classes.dropDownSelect)} name="Language">
-      <option value="en">ENGLISH</option>
-      <option value="fr">FRENCH</option>
-      <option value="it">ITALIAN</option>
+      <option value="en">{t("dropdown.en")}</option>
+      <option value="fr">{t("dropdown.fr")}</option>
+      <option value="it">{t("dropdown.it")}</option>
     </select>
   );
 };
