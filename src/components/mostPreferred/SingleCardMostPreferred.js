1d494d921e70a6ac9a839e5b3eecdd388036379d
Mantas Navickis <navickis@gmail.com>
2023-10-13 12:50:15 +0200

    fixed carousel

--- src/components/mostPreferred/SingleCardMostPreferred.js
+++ src/components/mostPreferred/SingleCardMostPreferred.js
@@ -4,7 +4,7 @@ import classes from './MostPreferred.module.css';
 const SingleCardMostPreferred = ({ category }) => {
   return (
     <div className={classes.MostPreferredSingleCard}>
-      <img className={classes.MostPreferredSingleCardImage} src={category.image} alt={category.name} />
+      <img className={classes.MostPreferredSingleCardImage} src={category.imageUrl} alt={category.name} />
       <p className={classes.MostPreferredSingleCardP}>{category.name}</p>
     </div>
   );
