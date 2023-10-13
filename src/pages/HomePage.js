0a3841c405a0000cdf65cb3d0a548dda2f51c9ea
Isabelle <isabel.tartarelli@live.it>
2023-10-13 16:05:47 +0100

    Created mostPopular

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,28 +4,27 @@ import WeeklyFeatured from '../components/weeklyFeatured/WeeklyFeatured'
 import Footer from '../components/footer/FooterContainer'
 import RecentlyViewed from '../components/recentlyViewed/RecentlyViewed'
 import MostPreferred from '../components/mostPreferred/MostPreferred'
-import i18n from "../i18n";
+import MostPopular from '../components/mostPopularProducts/MostPopular'
+import i18n from '../i18n'
 import React, { useState } from 'react'
 
-
-
 const HomePage = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language); // i18n.language contains the language assigned to lng in i18n.js file.
-  
-  
-  return (
-    <>
-    <Context.Provider value={[selectedLanguage, setSelectedLanguage]}>
-      <NavBar />
-      <MainSlider />
-      <WeeklyFeatured />
-      <MostPreferred />
-      <RecentlyViewed />
-      <Footer />
-    </Context.Provider>
-    </>
-  )
+	const [selectedLanguage, setSelectedLanguage] = useState(i18n.language) // i18n.language contains the language assigned to lng in i18n.js file.
+
+	return (
+		<>
+			<Context.Provider value={[selectedLanguage, setSelectedLanguage]}>
+				<NavBar />
+				<MainSlider />
+				<WeeklyFeatured />
+				<MostPreferred />
+				<MostPopular />
+				<RecentlyViewed />
+				<Footer />
+			</Context.Provider>
+		</>
+	)
 }
 
 export default HomePage
-export const Context = React.createContext();
+export const Context = React.createContext()
