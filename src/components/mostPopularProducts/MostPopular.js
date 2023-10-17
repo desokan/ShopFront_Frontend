683782c05357774a0ab35d7ec5615ca5a664823f
desokan <thefdmm@gmail.com>
2023-10-17 13:15:30 +0100

    fixed eslint error

--- src/components/mostPopularProducts/MostPopular.js
+++ src/components/mostPopularProducts/MostPopular.js
@@ -5,40 +5,40 @@ import { useTranslation } from 'react-i18next';
 import i18n from "i18next";
 
 const MostPopular = () => {
-	const { t } = useTranslation();
-  
-	const [selectedTab, setSelectedTab] = useState('');
-  
-	useEffect(() => {
-	  if (i18n.language === 'it') {
-		setSelectedTab("TUTTI");
-	  } else if (i18n.language === "fr") {
-		setSelectedTab("TOUS");
-	  } else {
-		setSelectedTab("ALL");
-	  }
-	}, [i18n.language]);
-  
-	const productCategories = t('mostPopular.tabs', {
-	  returnObjects: true,
-	});
-  
-	const products = t('mostPopular.products', {
-	  returnObjects: true,
-	});
-  
-	const handleTabClick = (tabName) => {
-	  setSelectedTab(tabName);
-	};
+  const { t } = useTranslation();
+
+  const [selectedTab, setSelectedTab] = useState("");
+
+  useEffect(() => {
+    if (i18n.language === "it") {
+      setSelectedTab("TUTTI");
+    } else if (i18n.language === "fr") {
+      setSelectedTab("TOUS");
+    } else {
+      setSelectedTab("ALL");
+    }
+  }, []); // [i18n.language]
+
+  const productCategories = t("mostPopular.tabs", {
+    returnObjects: true,
+  });
+
+  const products = t("mostPopular.products", {
+    returnObjects: true,
+  });
+
+  const handleTabClick = (tabName) => {
+    setSelectedTab(tabName);
+  };
   return (
     <div className={classes.MostPopularContainer}>
       <div className={classes.MostPopularTitleContainer}>
         <div className={classes.MostPopularTitle}>
           <h2 className={classes.MostPopularTitleH3}>
-            {t('mostPopular.title')}
+            {t("mostPopular.title")}
           </h2>
           <p className={classes.MostPopularTitleP}>
-            {t('mostPopular.subTitle')}
+            {t("mostPopular.subTitle")}
           </p>
         </div>
       </div>
@@ -46,7 +46,9 @@ const MostPopular = () => {
         <ul className={classes.MostPopularUl}>
           {productCategories.map((category, index) => (
             <li
-              className={`${classes.tab} ${selectedTab === category.tabName ? classes.active : ''}`}
+              className={`${classes.tab} ${
+                selectedTab === category.tabName ? classes.active : ""
+              }`}
               key={index}
               onClick={() => handleTabClick(category.tabName)}
             >
@@ -57,7 +59,13 @@ const MostPopular = () => {
       </div>
       <div className={classes.MostPopularCards}>
         <Carousel
-          products={products.filter(product => selectedTab === 'ALL' || selectedTab === 'TUTTI' || selectedTab === 'TOUS' || product.mainCategory === selectedTab)}
+          products={products.filter(
+            (product) =>
+              selectedTab === "ALL" ||
+              selectedTab === "TUTTI" ||
+              selectedTab === "TOUS" ||
+              product.mainCategory === selectedTab
+          )}
         />
       </div>
     </div>
