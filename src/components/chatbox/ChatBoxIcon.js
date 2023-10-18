006b81fae9249bc645892cf4e881db92403596ad
carolina arruda <carolinacalouroarruda@gmail.com>
2023-10-18 12:17:54 +0200

    changed style

--- src/components/chatbox/ChatBoxIcon.js
+++ src/components/chatbox/ChatBoxIcon.js
@@ -2,43 +2,47 @@ import ChatIcon from "@mui/icons-material/Chat";
 import classes from "./ChatBoxIcon.module.css";
 import ChatBox from "react-simple-chatbot";
 import { wait } from "@testing-library/user-event/dist/utils";
+import { ThemeProvider } from "styled-components";
 
 const ChatBoxIcon = () => {
   const openChat = () => {
     console.log("hello");
   };
 
+  const theme = {
+    background: "#f5f8fb",
+    fontFamily: "Helvetica Neue",
+    headerBgColor: "#EF6C00",
+    headerFontColor: "#fff",
+    headerFontSize: "15px",
+    botBubbleColor: "#EF6C00",
+    botFontColor: "#fff",
+    userBubbleColor: "#fff",
+    userFontColor: "#4a4a4a",
+  };
+
   const steps = [
     {
-        id: "Greet",
-        message: "Hello, my name is ChattyChat",
-        trigger: "Ask Name"
+      id: "Greet",
+      message: "Hello, my name is ChattyChat",
+      trigger: "Ask Name",
     },
     {
-        id: "Ask Name",
-        message: "Please enter your name",
-        trigger: 'waiting1'
+      id: "Ask Name",
+      message: "Please enter your name",
+      trigger: "waiting1",
     },
     {
-        id: "waiting1",
-        user: true,
-        end: true
-    }
-  ]
+      id: "waiting1",
+      user: true,
+      end: true,
+    },
+  ];
   return (
     <>
-      <ChatBox steps ={steps}/>
-      <div className={classes.chatBoxContainer} onClick={openChat}>
-        <ChatIcon
-          sx={{
-            backgroundColor: "#eba37a",
-            color: "white",
-            width: "30px",
-            height: " 30px",
-            placeSelf: "center",
-          }}
-        />
-      </div>
+      <ThemeProvider theme={theme}>
+        <ChatBox steps={steps} floating={true} />
+      </ThemeProvider>
     </>
   );
 };
