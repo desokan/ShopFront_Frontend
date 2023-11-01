00df85b9705502090b10e50fa2ee598666c63443
carolina arruda <carolinacalouroarruda@gmail.com>
2023-11-01 17:08:41 +0100

    working on design

--- src/components/authentication/AccountMenu.js
+++ src/components/authentication/AccountMenu.js
@@ -1,114 +1,38 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import classes from "./Login.module.css";
 import BackDrop from "../util/Backdrop";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import CloseIcon from "../svgs/CloseIcon";
+import classes from './Account.module.css'
 
 const AccountMenu = ({ closeLogin, openRegister, user }) => {
-  const navigate = useNavigate();
-
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  console.log(user);
-
-
-
-
-
-
-  function handleChange(e) {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
-
   const handleCloseAccount = () => {
     closeLogin(false);
-
   };
 
-  const handleRegister = () => {
-    closeLogin(false);
-    openRegister(true);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    fetch(`http://localhost:4000/login`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(formData),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        localStorage.setItem("token", data.accessToken);
-
-      });
-
-    console.log("Redirecting to HomePage...");
-  };
   return (
     <>
-      <div className={classes.closeLoginForm}>
-        <p className={classes.loginText}>
+      <div className={classes.closeAccountForm}>
+        <p className={classes.accountText}>
           <b>WELCOME BACK, {user.username.toUpperCase()}</b>
         </p>
-        <button className={classes.closeLogin} onClick={handleCloseAccount}>
+        <button className={classes.closeAccount} onClick={handleCloseAccount}>
           <CloseIcon />
         </button>
       </div>
-      <form className={classes.loginForm} onSubmit={handleSubmit}>
-        <input
-          className={classes.textBox}
-          type="text"
-          placeholder="Username or email address *"
-          value={formData.email}
-          name="email"
-          onChange={(e) => handleChange(e)}
-        />
-
-        <input
-          className={classes.textBox}
-          type="password"
-          placeholder="Password *"
-          name="password"
-          value={formData.password}
-          onChange={(e) => handleChange(e)}
-        />
-
-        <div className={classes.inputInfo}>
-          <div className={classes.checkboxLabel}>
-            <input
-              className={classes.checkboxInput}
-              type="checkbox"
-              id="rememberMe"
-              onChange={handleChange}
-              name="rememberMe"
-            />{" "}
-            <label className={classes.checkboxLabel}>Remember Me</label>
-          </div>
-          <div>
-            <p className={classes.underline}>
-              <Link>Lost password?</Link>
-            </p>
-          </div>
-        </div>
-
-        <button className={classes.loginButton} type="submit">
-          LOG IN
-        </button>
-        <p className={classes.newAccount}>
-          No account yet?{" "}
-          <span>
-            <Link onClick={handleRegister}>Create Account</Link>
-          </span>
-        </p>
-      </form>
+      <div className={classes.accountContainer}>
+        <h1 className={classes.accountH1}>ACCOUNT DETAILS</h1>
+        <ul className={classes.accountUL}>
+          <li className={classes.accountLi}>DASHBORD</li>
+          <li className={classes.accountLi}>ORDERS</li>
+          <li className={classes.accountLi}>SETTINGS</li>
+          <li className={classes.accountLi}>WISHLIST</li>
+          <li className={classes.accountLi} >
+            LOGOUT
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
